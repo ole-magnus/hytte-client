66f787beb5a808983445e4609fce1c098825fbfc
Ole-Magnus <ole.magnus.aker@gmail.com>
2017-01-22 16:07:42 +0100

    ip changed

--- client.js
+++ client.js
@@ -5,31 +5,26 @@ var logger = require('winston');
 logger.add(logger.transports.File, { filename: 'logfile.log' });
 logger.remove(logger.transports.Console);
 
+
+var url = "http://192.168.0.11:3000/"
+var pathNorth = "upload/north"
+var pathWest = "upload/west"
+var pathStatus = "status"
+
 var timeInterval = 3600 * 1000;
 var imageName = "lastImage.jpg"
 var cmd0 = 'fswebcam -d /dev/video0 -r 1280x720 --jpeg 90 -D 3 -S 13 ' + imageName;
 var cmd1 = 'fswebcam -d /dev/video1 -r 1280x720 --jpeg 90 -D 3 -S 13 ' + imageName;
-var camera1 = "1stCamera";
-var camera2 = "2ndCamera";
+var camera1 = "north";
+var camera2 = "west";
 
-request(
-    { method: 'GET'
-    , uri: 'http://178.62.193.16:8080/upload/interval'
-    }
-  , function (error, response, body) {
-
-      if(!error) {
-        //timeInterval = parseInt(body) * 1000;
-	logger.log("info", "TimeInterval is set to " + timeInterval);
-	startJob();
-      }
-    });
+startJob()
 
 function startJob() {
-	fireRequest();
-	setInterval(function(){
-	  fireRequest();
-	}, timeInterval);
+  fireRequest();
+  setInterval(function(){
+    fireRequest();
+  }, timeInterval);
 }
 
 
@@ -37,34 +32,34 @@ function startJob() {
 function fireRequest() {
   request(
     { method: 'GET'
-    , uri: 'http://178.62.193.16:8080/upload/status'
+    , uri: url + pathStatus
     }
   , function (error, response, body) {
 
       if(!error) {
         if(body == "ON") {
           logger.log("info", "Received upload status: ON");
-          snapPicture(cmd0, camera1);
+          snapPicture(cmd0, camera1, url+pathNorth);
         }
       } else {
-	logger.log("error", "Error when uploading picture");
-    	}
-	});
+  logger.log("error", "Error when uploading picture");
+      }
+  });
 }
 
 var exec = require('child_process').exec;
 
 
 
-function snapPicture(cmd, camera) {
+function snapPicture(cmd, camera, url) {
   exec(cmd, function(error, stfout, stderr) {
     logger.log("info", "Picture taken. CMD: " + cmd);
-    postImage(__dirname + "/" + imageName, camera);
+    postImage(__dirname + "/" + imageName, camera, url);
   });
 }
 
 
-function postImage(imagePath, camera) {
+function postImage(imagePath, camera, url) {
   var formData = {
     // Pass a simple key-value pair
     name: camera,
@@ -72,14 +67,14 @@ function postImage(imagePath, camera) {
     file: fs.createReadStream(imagePath)
   };
 
-  request.post({url:'http://178.62.193.16:8080/upload', formData: formData}, function optionalCallback(err, httpResponse, body) {
+  request.post({url: url, formData: formData}, function optionalCallback(err, httpResponse, body) {
     if (err) {
-    	return logger.log("error", "Error occured while uploading: " + err);
+      return logger.log("error", "Error occured while uploading: " + err);
     }
     logger.log('info', 'Upload successful!  Server responded with:', body);
     if(camera == camera1) {
         logger.log("info", "Snapping from camera 2");
-    	snapPicture(cmd1, camera2);
+      snapPicture(cmd1, camera2, url+pathWest);
     }
    });
 
