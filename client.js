3811ec04e804c3041102990613161c1c210cfd58
Ole-Magnus Aker <ole.magnus.aker@schibsted.com>
2017-04-16 17:03:54 +0200

    added cron

--- client.js
+++ client.js
@@ -1,6 +1,23 @@
 var request = require('request');
 var fs = require('fs');
 var logger = require('winston');
+var CronJob = require('cron').CronJob
+
+// Every x minute
+const cronPattern = '0 0/10 * 1/1 * ? *'
+
+var job = new CronJob(cronPattern, function() {
+
+  // Do request
+  fireRequest()
+
+}, function() {
+
+  // Job stopped
+  const date = new Date()
+  logger.log("error", "Job stopped: " + date.toString())
+}
+);
 
 logger.add(logger.transports.File, { filename: 'logfile.log' });
 logger.remove(logger.transports.Console);
@@ -11,23 +28,12 @@ var pathNorth = "upload/north"
 var pathWest = "upload/west"
 var pathStatus = "status"
 
-var timeInterval = 2700 * 1000;
-var imageName = "lastImage.jpg"
-var cmd0 = 'fswebcam -d /dev/video0 -r 1280x720 --jpeg 90 -D 3 -S 13 ' + imageName;
-var cmd1 = 'fswebcam -d /dev/video1 -r 1280x720 --jpeg 90 -D 3 -S 13 ' + imageName;
-var camera1 = "north";
-var camera2 = "west";
-
-startJob()
-
-function startJob() {
-  fireRequest();
-  setInterval(function(){
-    fireRequest();
-  }, timeInterval);
-}
-
-
+const timeInterval = 2700 * 1000;
+const imageName = "lastImage.jpg"
+const cmd0 = 'fswebcam -d /dev/video0 -r 1280x720 --jpeg 90 -D 3 -S 13 ' + imageName;
+const cmd1 = 'fswebcam -d /dev/video1 -r 1280x720 --jpeg 90 -D 3 -S 13 ' + imageName;
+const camera1 = "north";
+const camera2 = "west";
 
 function fireRequest() {
   request(
@@ -42,14 +48,12 @@ function fireRequest() {
           snapPicture(cmd0, camera1, baseUrl+pathNorth);
         }
       } else {
-  logger.log("error", "Error when uploading picture");
+        logger.log("error", "Error on status request");
       }
   });
 }
 
 var exec = require('child_process').exec;
-
-
 
 function snapPicture(cmd, camera, url) {
   exec(cmd, function(error, stfout, stderr) {
@@ -69,11 +73,12 @@ function postImage(imagePath, camera, url) {
 
   request.post({url: url, formData: formData}, function optionalCallback(err, httpResponse, body) {
     if (err) {
-      return logger.log("error", "Error occured while uploading: " + err);
+      logger.log("error", "Error occured while uploading: " + err);
+    } else {
+      logger.log('info', 'Upload successful!  Server responded with:', body);
     }
-    logger.log('info', 'Upload successful!  Server responded with:', body);
     if(camera == camera1) {
-        logger.log("info", "Snapping from camera 2");
+      logger.log("info", "Snapping from camera 2");
       snapPicture(cmd1, camera2, baseUrl+pathWest);
     }
    });
